0e5353fbe15f54488119c497e818414d3c205ca0
stickler-ci <support@stickler-ci.com>
2019-06-10 17:00:25 +0000

    Fixing style errors.

--- main.rb
+++ main.rb
@@ -75,38 +75,38 @@ module Enumerable
 end
 
 # *** test for my_each ***
-(%w[den soft filter delter]).my_each { |item| print "item: #{item}" }
+%w[den soft filter delter].my_each { |item| print "item: #{item}" }
 #=> item: den,item: softitem: filter,item: delter
 
 # *** test for my_each_with_index ***
 print "\n"
-([4, 5, 8, 20]).my_each_with_index { |index, value| print "#{index}: #{value}  " }
+[4, 5, 8, 20].my_each_with_index { |index, value| print "#{index}: #{value}  " }
 print "\n"
 #=> 0: 4  1: 5  2: 8  3: 20
 
 # *** test for my_select ***
-print ([2, 8, 4, 9, 5, 7, 4]).my_select { |value| value > 5 }
+print [2, 8, 4, 9, 5, 7, 4].my_select { |value| value > 5 }
 print "\n"
 #=> [8, 9, 7]
 
 # *** test for my_all ***
-print ([4, 6, 1, 8]).my_all? { |val| val > 1 }
+print [4, 6, 1, 8].my_all? { |val| val > 1 }
 print "\n"
 #=> false
 
 # *** test for my_any ***
-result = (%w[life death immortal clean]).my_any? { |val| val.length > 9 }
+result = %w[life death immortal clean].my_any? { |val| val.length > 9 }
 print result
 print "\n"
 #=> false
 
 # *** test for my_none ***
-print ([4, 6, 1, 8]).my_none? { |val| val < 1 }
+print [4, 6, 1, 8].my_none? { |val| val < 1 }
 print "\n"
 #=> true
 
 # *** test for my_count ***
-print ([4, 6, 1, 8]).my_count
+print [4, 6, 1, 8].my_count
 print "\n"
 #=> 4
 
@@ -121,6 +121,6 @@ puts multiply_els([2, 4, 5])
 
 # **** test my_map using proc****
 item = ->(val) { val > 5 }
-result = ([2, 8, 4, 9, 5, 7, 4]).my_map(&item)
+result = [2, 8, 4, 9, 5, 7, 4].my_map(&item)
 puts "new_array: #{result}"
 #=> new_array: [false, true, false, true, false, true, false]
