9c30abb6d8b9d74bbb13ba0a08f62264a904b64d
wathigo <wathigosimon@gmail.com>
2019-06-15 17:57:16 -0400

    Add tests for Enumerable methods

--- main.rb
+++ main.rb
@@ -3,51 +3,87 @@
 module Enumerable
   # module that defines common enumerable methods
   def my_each
-    (0...length).each do |i|
-      yield(self[i])
+    if block_given?
+      (0...length).each do |i|
+        yield(self[i])
+      end
+    else
+      to_enum(:my_each)
     end
   end
 
   def my_each_with_index
-    (0...length).each do |i|
-      yield(i, self[i])
+    if block_given?
+      (0...length).each do |i|
+        yield(self[i], i)
+      end
+    else
+      to_enum(:my_each_with_index)
     end
   end
 
   def my_select
     items = []
-    (0...length).each do |i|
-      item = yield(self[i])
-      items.append(self[i]) if item
+    if block_given?
+      (0...length).each do |i|
+        item = yield(self[i])
+        items.append(self[i]) if item
+      end
+    else
+      to_enum(:my_select)
     end
     items
   end
 
   def my_all?
     return true if self == []
-
-    (0...length).each do |i|
-      truth = yield(self[i])
-      return truth if truth == false
+    truth = true
+    if block_given?
+      (0...length).each do |i|
+        truth = yield(self[i])
+        return truth if truth == false
+      end
+    else
+      (0...length).each do |i|
+        if self[i] == nil || self[i] == false
+          return false
+        end
+      end
+      return true
     end
     truth
   end
 
   def my_any?
     truth = false
-    (0...length).each do |i|
-      truth = yield(self[i])
-      return true if truth == true
+    if block_given?
+      (0...length).each do |i|
+        truth = yield(self[i])
+        return true if truth == true
+      end
+    else
+      (0...length).each do |i|
+        if self[i] != nil && self[i] != false
+          return true
+        end
+      end
     end
     truth
   end
 
   def my_none?
     return true if self == []
-
-    (0...length).each do |i|
-      truth = yield(self[i])
-      return false if truth == true
+    if block_given?
+      (0...length).each do |i|
+        truth = yield(self[i])
+        return false if truth == true
+      end
+    else
+      (0...length).each do |i|
+        if self[i] != false && self[i] != nil
+          return false
+        end
+      end
     end
     true
   end
@@ -74,53 +110,8 @@ module Enumerable
   end
 end
 
-# *** test for my_each ***
-%w[den soft filter delter].my_each { |item| print "item: #{item}" }
-#=> item: den,item: softitem: filter,item: delter
-
-# *** test for my_each_with_index ***
-print "\n"
-[4, 5, 8, 20].my_each_with_index { |index, value| print "#{index}: #{value}  " }
-print "\n"
-#=> 0: 4  1: 5  2: 8  3: 20
-
-# *** test for my_select ***
-print [2, 8, 4, 9, 5, 7, 4].my_select { |value| value > 5 }
-print "\n"
-#=> [8, 9, 7]
-
-# *** test for my_all ***
-print [4, 6, 1, 8].my_all? { |val| val > 1 }
-print "\n"
-#=> false
-
-# *** test for my_any ***
-result = %w[life death immortal clean].my_any? { |val| val.length > 9 }
-print result
-print "\n"
-#=> false
-
-# *** test for my_none ***
-print [4, 6, 1, 8].my_none? { |val| val < 1 }
-print "\n"
-#=> true
-
-# *** test for my_count ***
-print [4, 6, 1, 8].my_count
-print "\n"
-#=> 4
-
-# *** test my_inject ***
 def multiply_els(arr)
   arr.my_inject do |prod, val|
     prod * val
   end
 end
-puts multiply_els([2, 4, 5])
-#=> 40
-
-# **** test my_map using proc****
-item = ->(val) { val > 5 }
-result = [2, 8, 4, 9, 5, 7, 4].my_map(&item)
-puts "new_array: #{result}"
-#=> new_array: [false, true, false, true, false, true, false]
